fd2516f9e39b49c748f42c2812ac0daca3d0990a
Alasdair Rothnie <alasdair.rothnie@gmail.com>
2019-04-07 15:49:54 -0700

    my second commit

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
-
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
-console("vaishnavi")
 
 
